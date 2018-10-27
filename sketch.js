878c2d135f5ea634ac49e3b4f4385feea68800ec
doodybrains <doodybrainz@gmail.com>
2018-10-27 15:14:21 -0400

    clean up

--- sketch.js
+++ sketch.js
@@ -1,91 +1,65 @@
 const ansi = require('ansi-escape-sequences');
 
+let meme = [
+  'has anyone', 'really been',
+  'far', 'even as',
+  'decided to use',
+  'even go','want to do', 'look more like?'
+];
+
+let all = [
+  'how far is a click', 'even as your soul prospers', 'has anyone',
+  'really been', 'decided to use', 'has anyone won the mega millions',
+  'even go', 'look more like?', 'ive really been on a bender lately and it shows',
+  'want to do',  'who decided to use bc and ad', 'i even go to work', 'far',
+  'i want to download', 'even as', 'do i look more like i do in the mirror or in pictures?'
+];
+
 setup();
 
 function setup() {
   const interval = setInterval(function() {
-    makeAText();
-  }, 1200);
-
-  setTimeout(function() {
-    clearInterval(interval);
-  }, 70000)
+    const ranSentence = randomizeArray(all);
+    
+    // position text between 0 and 30 rows, 0 and 80 columns
+    process.stdout.write(antsyPosition(soRandom(-30), soRandom(80)));
+    process.stdout.write(antsyFormat(ranSentence, 'gray'));
+    setTimeout(function() {
+      makeAText(ranSentence);
+    }, 20000)
+  }, 500);
 }
 
-function makeAText() {
-
-  let colors = ["yellow", "magenta", "cyan", "blue", "green"];
-
-  let meme = [
-    'has anyone', 'really been',
-    'far', 'even as',
-    'decided to use',
-    'even go','want to do', 'look more like?'
-  ];
-
-  let sentences = [
-    'has anyone won the mega millions',
-    'ive really been on a bender lately and it shows',
-    'how far is a click',
-    'even as your soul prospers',
-    'who decided to use bc and ad',
-    'i even go to work',
-    'i want to download',
-    'do i look more like i do in the mirror or in pictures?'
-  ];
-
-  const ranColor = randomizeArray(colors);
-  const ranPhrase = randomizeArray(meme);
-  const ranSentence = randomizeArray(sentences);
-
-  process.stdout.write(antsyPosition(soRandom(20), soRandom(120)));
-  process.stdout.write(antsyFormat(ranSentence, 'gray'));
-
-	process.stdout.write(antsyPosition(soRandom(20), soRandom(80)));
-  process.stdout.write(antsyFormat(ranPhrase, 'gray'));
-
-  setTimeout(function() {
-    switch(ranPhrase) {
-      case 'has anyone':
-        highlightPhrase(5,53,'has anyone','red', 12000);
-        writeBackgroundText(5,53,'has anyone');
-        break;
-      case 'really been':
-        highlightPhrase(7,57,'really been','green', 16000);
-        writeBackgroundText(7,57,'really been');
-        break;
-      case 'far':
-        highlightPhrase(9,55,'far','yellow', 19000);
-        writeBackgroundText(9,55,'far');
-      case 'even as':
-        highlightPhrase(11,60,'evan as','blue', 22000);
-        writeBackgroundText(11,60,'evan as');
-      case 'decided to use':
-        highlightPhrase(13,58,'decided to use','magenta', 25000);
-        writeBackgroundText(13,58,'decided to use');
-      case 'even go':
-        highlightPhrase(15,62,'even go','cyan', 29000);
-        writeBackgroundText(15,62,'even go');
-      case 'want to do':
-        highlightPhrase(17,62,'want to do','red', 35000);
-        writeBackgroundText(17,62,'want to do');
-      case 'look more like?':
-        highlightPhrase(18,50,'look more like?','green', 40000);
-        writeBackgroundText(18,50,'look more like?');
-    }
-  })
+function makeAText(ranSentence) {
+  switch(ranSentence) {
+    case 'has anyone':
+      highlightPhrase(14,33,'has anyone','red');
+      break;
+    case 'really been':
+      highlightPhrase(15,37,'really been','green');
+      break;
+    case 'far':
+      highlightPhrase(16,35,'far','yellow');
+    case 'even as':
+      highlightPhrase(17,40,'evan as','blue');
+    case 'decided to use':
+      highlightPhrase(18,45,'decided to use','magenta');
+    case 'even go':
+      highlightPhrase(19,42,'even go','cyan');
+    case 'want to do':
+      highlightPhrase(20,42,'want to do','red');
+    case 'look more like?':
+      highlightPhrase(21,30,'look more like?','green');
+  }
 }
 
-function highlightPhrase(col, row, phrase, color, delay) {
+function highlightPhrase(col, row, phrase, color) {
+  process.stdout.write(antsyPosition(col, row));
+  process.stdout.write(antsyFormat(phrase, 'gray'));
   setTimeout(function() {
     process.stdout.write(antsyPosition(col, row));
     process.stdout.write(antsyFormat(phrase, color));
-  }, delay)
-}
-
-function writeBackgroundText(col, row, phrase) {
-  process.stdout.write(antsyPosition(col, row));
-  process.stdout.write(antsyFormat(phrase, 'gray'));
+  }, 1000 + col * 5)
 }
 
 function randomizeArray(array) {
